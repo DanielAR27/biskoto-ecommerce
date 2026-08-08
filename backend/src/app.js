437eb60b6409871c0799bdf1c4607935acfb18b1
Daniel <copperdar27@gmail.com>
2026-08-07 18:30:40 -0600

    limpiar comentarios residuales

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,12 +25,11 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-// 🔍 AGREGAR ESTAS LÍNEAS AQUÍ
 app.use((req, res, next) => {
   console.log(`🔵 ${req.method} ${req.url}`);
   next();
 });
-// 🔍 FIN
+
 
 // Ruta rápida para mantener el servidor despierto (Cron Job)
 app.get("/ping", (req, res) => {
